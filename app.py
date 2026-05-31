c2452f8fca1b032bd34f11aa5e9fdec36fe4b120
ant240 <101406234+ant240@users.noreply.github.com>
2026-05-31 18:12:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,44 @@
 import streamlit as st
-import inspect
 import cianparser
+import random
 
-st.title("Диагностика CianParser")
+st.title("Проверка CianParser через прокси")
+
+with open("proxies.txt") as f:
+    proxies = [x.strip() for x in f if x.strip()]
+
+proxy = random.choice(proxies)
+
+st.write("Прокси:")
+st.code(proxy)
 
 try:
-    st.write("Версия библиотеки загружена")
 
-    signature = inspect.signature(cianparser.CianParser)
+    parser = cianparser.CianParser(
+        location="Москва",
+        proxies={
+            "http": proxy,
+            "https": proxy
+        }
+    )
 
-    st.code(str(signature))
+    st.success("Парсер создался")
+
+    data = parser.get_flats(
+        deal_type="sale",
+        rooms=(1,),
+        additional_settings={
+            "start_page": 1,
+            "end_page": 1
+        }
+    )
+
+    st.write("Количество объектов:")
+
+    st.write(len(data))
+
+    if len(data):
+        st.json(data[0])
 
 except Exception as e:
     st.error(str(e))
